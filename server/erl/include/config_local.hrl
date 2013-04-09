78fa3930792a6d1399eeef861fc4c323099ea0f0
slezakattack <michael_slezak08@hotmail.com>
2013-04-09 15:45:23 -0700

    config_local.hrl was not added because its listed in .gitignore.. -__-

---
+++ server/erl/include/config_local.hrl
@@ -0,0 +1,9 @@
+%% exact copy of config_local.json
+{config,
+    [{port, 8000},
+    {debug_level, "info"},
+    {nb_players_per_world, 200},
+    {nb_worlds, 5},
+    {map_filepath, "./server/maps/world_server.json"},
+    {metrics_enabled, false}]
+}.
